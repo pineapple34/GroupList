27da2d3feb2147a753a2c18596b9fbc3bc8c2151
pineapple34 <pineapple6934@gmail.com>
2020-12-03 21:47:43 +0500

    Добавьте файлы проекта.

---
+++ GroupList/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace GroupList
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> group1;
+        ObservableCollection<string> group2;
+        ObservableCollection<string> group3;
+        ObservableCollection<string> group4;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void addStudent_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void changeStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
